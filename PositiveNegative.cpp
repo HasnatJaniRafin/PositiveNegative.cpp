4761974a582e379381c5b0278c3d5469f7b5291a
HASNAT JANI RAFIN <77789741+HasnatJaniRafin@users.noreply.github.com>
2021-06-11 09:54:51 +0800

    Add files via upload

---
+++ PositiveNegative.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std ;
+int main ()
+
+{
+    int x ;
+    cout << " Enter an Integer Number : " ;
+    cin >> x ;
+
+    if (x >= 0)
+             cout << " This number is Positive " <<endl ;
+    else 
+         cout << " This number is Negative " <<endl ;         
+                 
+
+    return 0;
+}
+
